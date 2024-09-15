5647ab1ae3762c7344a48f2d70f5760d6bdcc856
juliatibes <julia.tibes@gmail.com>
2024-09-15 19:01:18 -0300

    estilização - cadastro periodo

--- src/pages/CadastroPeriodo/index.tsx
+++ src/pages/CadastroPeriodo/index.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import InputPadrao from "../../components/InputPadrao";
 import "./index.css";
 import SideMenu from "../../components/SideMenu";
+import BotaoPadrao from "../../components/BotaoPadrao";
 
 const CadastroPeriodo: FC = () => {
   return (
@@ -9,14 +10,18 @@ const CadastroPeriodo: FC = () => {
       <main>
         <SideMenu />
         <div className="cadastro-periodo">
-          <div className="cadastro-periodo-nome-email">
-            <InputPadrao label={"Nome completo"} type={"text"} />
-            <InputPadrao label={"E-mail"} type={"email"} />
+          <div className="titulo">
+            <h3 className="titulo-pagina">Período </h3>
+            <span> - cadastro</span>
           </div>
-          <div className="cadastro-periodo-celular-cpf">
-            <InputPadrao label={"Celular"} type={"number"} />
-            <InputPadrao label={"CPF"} type={"number"} />
+          <div className="cadastro-periodo-nome">
+            <InputPadrao label={"Nome do período"} type={"text"} />
           </div>
+          <div className="cadastro-periodo-data">
+            <InputPadrao label={"Data inicial"} type={"date"} />
+            <InputPadrao label={"Data final"} type={"date"} />
+          </div>
+          <BotaoPadrao label={"Salvar"}/>
         </div>
       </main>
     </>
