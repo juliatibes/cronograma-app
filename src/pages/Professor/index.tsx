8a9e9084695a8cd00bd854afc3b7a069713569c0
barbstefani <barbstefani02@gmail.com>
2024-10-15 21:42:12 -0300

    botão padrão e header

--- src/pages/Professor/index.tsx
+++ src/pages/Professor/index.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
 import BotaoPadrao from "../../components/BotaoPadrao";
 import SideMenu from "../../components/SideMenu";
-import "./index.css"; 
+import "./index.css";
 import { useNavigate } from "react-router-dom";
 import { AlternateEmail, CalendarMonth, Phone } from "@mui/icons-material";
 
 const Professor: FC = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleAddClick = () => {
     navigate("/cadastroprofessor");
@@ -16,42 +16,101 @@ const Professor: FC = () => {
     <main>
       <SideMenu />
       <div className="cadastro-professor">
-        <h2>Professor</h2>
-        <div className="botao-adicionar">
-          <BotaoPadrao label={"Adicionar"} onClick={handleAddClick} />
+        <div className="header-professor">
+          <h2>Professor</h2>
+          <div className="botao-adicionar">
+            <BotaoPadrao label={"Adicionar"} onClick={handleAddClick} />
+          </div>
         </div>
 
         <div className="professores-grid">
           {/* Card de informações do professor */}
           <div className="professor-card">
             <h3>Muriel de Muriel Muriel Muriel</h3>
-            <p><strong><AlternateEmail /> </strong>MurielMuriel@gmail.commmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
-            <p><strong><Phone /> </strong>(48) 99867-4282</p>
-            <p><strong>< CalendarMonth /> </strong>Terça, sexta</p>
+            <p>
+              <strong>
+                <AlternateEmail />{" "}
+              </strong>
+              MurielMuriel@gmail.commmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+            </p>
+            <p>
+              <strong>
+                <Phone />{" "}
+              </strong>
+              (48) 99867-4282
+            </p>
+            <p>
+              <strong>
+                <CalendarMonth />{" "}
+              </strong>
+              Terça, sexta
+            </p>
           </div>
           <div className="professor-card">
             <h3>Maria Souza</h3>
-            <p><strong>Email: </strong>maria.souza@universidade.com</p>
-            <p><strong>Telefone: </strong>(21) 98888-1234</p>
-            <p><strong>Dias disponiveis: </strong>Segunda, Sexta</p>
+            <p>
+              <strong>Email: </strong>maria.souza@universidade.com
+            </p>
+            <p>
+              <strong>Telefone: </strong>(21) 98888-1234
+            </p>
+            <p>
+              <strong>Dias disponiveis: </strong>Segunda, Sexta
+            </p>
           </div>
           <div className="professor-card">
             <h3>Muriel de Muriel Muriel</h3>
-            <p><strong><AlternateEmail /> </strong>MurielMuriel@gmail.com</p>
-            <p><strong><Phone /> </strong>(48) 99867-4282</p>
-            <p><strong>< CalendarMonth /> </strong>Terça, sexta</p>
+            <p>
+              <strong>
+                <AlternateEmail />{" "}
+              </strong>
+              MurielMuriel@gmail.com
+            </p>
+            <p>
+              <strong>
+                <Phone />{" "}
+              </strong>
+              (48) 99867-4282
+            </p>
+            <p>
+              <strong>
+                <CalendarMonth />{" "}
+              </strong>
+              Terça, sexta
+            </p>
           </div>
           <div className="professor-card">
             <h3>Muriel de Muriel Muriel</h3>
-            <p><strong><AlternateEmail /> </strong>MurielMuriel@gmail.com</p>
-            <p><strong><Phone /> </strong>(48) 99867-4282</p>
-            <p><strong>< CalendarMonth /> </strong>Terça, sexta</p>
+            <p>
+              <strong>
+                <AlternateEmail />{" "}
+              </strong>
+              MurielMuriel@gmail.com
+            </p>
+            <p>
+              <strong>
+                <Phone />{" "}
+              </strong>
+              (48) 99867-4282
+            </p>
+            <p>
+              <strong>
+                <CalendarMonth />{" "}
+              </strong>
+              Terça, sexta
+            </p>
           </div>
           <div className="professor-card">
             <h3>Paulo Fernandes</h3>
-            <p><strong>Email: </strong>paulo.fernandes@universidade.com</p>
-            <p><strong>Telefone: </strong>(31) 97777-9876</p>
-            <p><strong>Dias disponiveis: </strong>Quinta, Sábado</p>
+            <p>
+              <strong>Email: </strong>paulo.fernandes@universidade.com
+            </p>
+            <p>
+              <strong>Telefone: </strong>(31) 97777-9876
+            </p>
+            <p>
+              <strong>Dias disponiveis: </strong>Quinta, Sábado
+            </p>
           </div>
         </div>
       </div>
